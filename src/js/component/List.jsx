5a96c7fb5a744aecd17d212c040e9d710843e930
atxoupi <andres.hermelo@gmail.com>
2022-06-10 09:55:06 +0000

    Funcionalidad lista

---
+++ src/js/component/List.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const List = ({ lista, setLista }) => {
+	function borrar(name) {
+		setLista(lista.filter((item) => item !== name));
+	}
+
+	const listItems = lista.map((tarea, i) => (
+		<li key={i} className="list-group-item m-auto w-50 d-flex">
+			<div className="w-100">{tarea}</div>
+			<div className="d-flex justify-content-end">
+				<button
+					className="btn btn-outline-danger"
+					type="button"
+					onClick={() => borrar(tarea)}>
+					<i className="fa-solid fa-trash"></i>
+				</button>
+			</div>
+		</li>
+	));
+	return (
+		<>
+			<ul className="list-group">{listItems}</ul>
+		</>
+	);
+};
+
+export default List;
